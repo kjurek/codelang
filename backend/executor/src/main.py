abba75e25f898782c771ae5702bd7871fbcf9dec
Karol Jurek <kjurek123@gmail.com>
2021-01-05 08:46:36 +0100

    Add build and execute buttons

--- backend/executor/src/main.py
+++ backend/executor/src/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import logging
 
 from src import handlers, schema
 
 logging.basicConfig(level=logging.DEBUG)
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods="*",
+    allow_headers="*"
+)
 
 
 @app.post("/compile", response_model=schema.CompileResponse)
--- frontend/html/lib/main_editor.js
+++ frontend/html/lib/main_editor.js
@@ -11,28 +11,61 @@ function showCompletionDetails(completion) {
     }
 }
 
+function sendCompletionsRequest(line_num, column_num, contents) {
+    request_data = {
+        "file_name": "code.cpp",
+        "file_type": "cpp",
+        "line_num": line_num,
+        "column_num": column_num,
+        "contents": contents,
+    };
+
+    return $.ajax({
+        type: "POST",
+        url: "http://localhost:8081/completions",
+        dataType: "json",
+        async: false,
+        data: JSON.stringify(request_data)
+    });
+}
+
+function sendCompileRequest(session_id, contents, flags) {
+    request_data = {
+        "session_id": session_id,
+        "contents": contents,
+        "flags": flags,
+    };
+
+    return $.ajax({
+        type: "POST",
+        url: "http://localhost:8082/compile",
+        dataType: "json",
+        async: false,
+        data: JSON.stringify(request_data),
+    });
+}
+
+function sendExecuteRequest(session_id) {
+    request_data = {
+        "session_id": session_id,
+        "arguments": [],
+    };
+
+    return $.ajax({
+        type: "POST",
+        url: "http://localhost:8082/execute",
+        dataType: "json",
+        async: false,
+        data: JSON.stringify(request_data),
+    });
+}
+
 function showCompletions(editor) {
     CodeMirror.showHint(editor, function () {
         const cursor = editor.getCursor();
         const end = cursor.ch;
         const line = cursor.line;
-
-        request_data = {
-            "file_name": "code.cpp",
-            "file_type": "cpp",
-            "line_num": line + 1,
-            "column_num": end + 1,
-            "contents": editor.getValue()
-        };
-
-        response = $.ajax({
-            type: "POST",
-            url: "http://localhost:8081/completions",
-            dataType: "json",
-            async: false,
-            data: JSON.stringify(request_data)
-        });
-
+        const response = sendCompletionsRequest(line + 1, end + 1, editor.getValue());
         const completion_start = response.responseJSON["completion_start_column"];
         const completion_offset = end + 1 - completion_start;
         const completions = $.map(response.responseJSON["completions"], function (x) {
@@ -40,7 +73,7 @@ function showCompletions(editor) {
                 displayText: x["menu_text"],
                 text: x["insertion_text"].substring(completion_offset),
                 signature: x["detailed_info"],
-                hint: function(editor, completions, selected) {
+                hint: function (editor, completions, selected) {
                     showCompletionDetails(selected);
                     editor.replaceRange(selected.text, completions.from);
                 }
@@ -53,6 +86,28 @@ function showCompletions(editor) {
 
         }
     }, { completeSingle: false });
+}
+
+function compileCode(editor) {
+    const session_id = "dummy_session";
+    const response = sendCompileRequest(session_id, editor.getValue(), ["-std=c++2a"]).responseJSON;
+    var compilationDetails = $("#compilation-details");
+    compilationDetails.empty();
+    compilationDetails.append($("<li>").text("stdout: " + response["stdout"]));
+    compilationDetails.append($("<li>").text("stderr: " + response["stderr"]));
+    compilationDetails.append($("<li>").text("returncode: " + response["returncode"]));
+}
+
+function executeCode(editor) {
+    const session_id = "dummy_session";
+    const response = sendExecuteRequest(session_id, editor.getValue()).responseJSON;
+    var executionDetails = $("#execution-details");
+    executionDetails.empty();
+    executionDetails.append([
+        $("<li>").text("stdout: " + response["stdout"]),
+        $("<li>").text("stderr: " + response["stderr"]),
+        $("<li>").text("returncode: " + response["returncode"])
+    ]);
 }
 
 (function () {
@@ -87,5 +142,13 @@ function showCompletions(editor) {
                 showCompletionDetails(completion);
             });
         }
+    });
+
+    $("#build-button").click(function() {
+        compileCode(editor);
+    });
+
+    $("#execute-button").click(function() {
+        executeCode(editor);
     });
 })()
