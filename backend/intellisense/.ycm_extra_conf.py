418f23b1177386e867633f2c95e33a317d8984a4
Karol Jurek <kjurek123@gmail.com>
2024-09-22 22:38:58 +0200

    fix bugs

--- backend/intellisense/.ycm_extra_conf.py
+++ backend/intellisense/.ycm_extra_conf.py
@@ -1,4 +1,4 @@
 def Settings( **kwargs ):
   return {
-    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-std=c++20', "-isystem", "/usr/include/c++/8"],
+    'flags': [ '-x', 'c++', '-Wall', '-Wextra', '-Werror', '-std=c++20', "-isystem", "/usr/include/c++/12"],
   }
